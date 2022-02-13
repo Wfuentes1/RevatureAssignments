7e5cae5cfc5e6bedca4e384aaac3e58778be4c7d
William A Fuentes <fuenteswilliam0326@gmail.com>
2022-02-13 13:45:47 -0800

    Add files via upload

--- Project1/src/com/company/Bank.java
+++ Project1/src/com/company/Bank.java
@@ -1,11 +1,12 @@
 package com.company;
 import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static java.lang.Integer.*;
 
 
-class Customer{
+class User{
     int balance;
     String name;
     int account_id;
@@ -21,6 +22,7 @@ class Employee{
 public class Bank {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
 	// write your code here
     Scanner input=new Scanner(System.in);
         System.out.println("Welcome to Revature Bank. Are you a 1) New Customer 2)Existing Customer or 3) Employee?");
@@ -69,31 +71,92 @@ public class Bank {
     }
 
     private static void new_Customer() throws ClassNotFoundException, SQLException {
-            System.out.println("Please name and balance you wish to add.");
-            Scanner scanner=new Scanner(System.in);
-            scanner.nextLine();
+           // System.out.println("Please give insert your full name.");
+           // Scanner scanner=new Scanner(System.in);
+
             String url = "jdbc:mysql://localhost:3306/project1";
             String username = "root";
             String password = "Brotherhood#22";
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
+        System.out.println("Welcome new customer! Please follow instructions to create your account.\n");
+        Scanner scanner=new Scanner(System.in);
 
 
-            String query="insert into customer(id, name,balance) values (?,?,?))";
+
+            String query="insert into customer(cust_id, name,balance) values (?,?,?)";
             PreparedStatement preparedStatement= connection.prepareStatement(query);
             //set the values of the query
+        System.out.println("Please make your id number.");
             String custId = scanner.nextLine();
+        System.out.println("Please give insert your full name.");
             String custName = scanner.nextLine();
+        System.out.println("Please give insert your starting amount to deposit.");
             String custBala = scanner.nextLine();
 
+       // System.out.println("Please give id number.");
             preparedStatement.setInt(1, parseInt(custId));
+      //  System.out.println("Please give insert your full name.");
             preparedStatement.setString(2, custName);
+       // System.out.println("Please give insert your amount to deposit.");
             preparedStatement.setInt(3, (parseInt(custBala)));
 
             int rowAffected=preparedStatement.executeUpdate();
-            System.out.println("("+rowAffected+") row affected");
+           System.out.println("("+rowAffected+") row affected");
+
+        System.out.println("Would like to make a 1)Deposit 2)Withdraw 3)Transfer or 4)Quit");
+        Scanner scan=new Scanner(System.in);
+        int user= scan.nextInt();
+        switch(user){
+            case 1:
+                Deposit();
+                break;
+            case 2:
+               // Withdraw();
+                break;
+            case 3:
+                //Transfer();
+            case 4:
+                System.exit(0);
+                break;
+
+
 
         }
 
+
+
+        }
+
+    private static void Deposit() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/project1";
+        String username = "root";
+        String password = "Brotherhood#22";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement statement = connection.createStatement();
+
+        String query = "Update customer set balance= balance + (?) where cust_id=(?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        System.out.println("Please enter amount you wished to deposit.(Do not exceed over 10000)");
+
+        Scanner scanner = new Scanner(System.in);
+       // int amt = scanner.nextInt();
+        Integer custBala = scanner.nextInt();
+        int amount= custBala;
+        preparedStatement.setInt(1, custBala);
+        if (amount > 0 ) {
+            System.out.println("Please give your id number.");
+            Integer custId = scanner.nextInt();
+            preparedStatement.setInt(2, custId);
+            int rowAffected=preparedStatement.executeUpdate();
+            System.out.println("("+rowAffected+") row affected");
+        } else {
+            System.out.println("Invalid entry");
+            System.exit(0);
+
+
+        }
+    }
 
 }
